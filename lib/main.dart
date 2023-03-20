54bfa48542ccfb43e55fe8cdb8b46bcce071feb9
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2023-03-20 12:45:28 +0530

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,248 @@
 import 'package:flutter/material.dart';
+import 'models/job.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'models/global.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        color: backgroundColor,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(40),
+                  constraints: BoxConstraints.expand(height: 225),
+                  decoration: BoxDecoration(
+                      gradient: new LinearGradient(
+                          colors: [lightBlueIsh, lightGreen],
+                          begin: const FractionalOffset(1.0, 1.0),
+                          end: const FractionalOffset(0.2, 0.2),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp
+                      ),
+                      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight:  Radius.circular(30))
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.only(top: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Find Your New Job', style: titleStyleWhite,)
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 120),
+                  constraints: BoxConstraints.expand(height:200),
+                  child: ListView(
+                      padding: EdgeInsets.only(left: 40),
+                      scrollDirection: Axis.horizontal,
+                      children: getRecentJobs()
+                  ),
+                ),
+                Container(
+                  height: 500,
+                  margin: EdgeInsets.only(top: 300),
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 40),
+                        child: Text(
+                          "Explore New Opportunities",
+                          style: titileStyleBlack,
+                        ),
+                      ),
+                      Container(
+                        height: 400,
+                        child: ListView(
+                          children: getJobCategories(),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+  List<String> jobCategories = ["Sales", "Engineering", "Health", "Education", "Finance"];
+
+  Map jobCatToIcon = {
+    "Sales" : Icon(Icons.monetization_on, color: lightBlueIsh, size: 50,),
+    "Engineering" : Icon(Icons.settings, color: lightBlueIsh, size: 50),
+    "Health" : Icon(Icons.healing, color: lightBlueIsh, size: 50),
+    "Education" : Icon(Icons.search, color: lightBlueIsh, size: 50),
+    "Finance" : Icon(Icons.card_membership, color: lightBlueIsh, size: 50),
+  };
+
+  Widget getCategoryContainer(String categoryName) {
+    return new Container(
+      margin: EdgeInsets.only(right: 10, left: 10, bottom: 20),
+      height: 180,
+      width: 140,
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        boxShadow: [
+          new BoxShadow(
+            color: Colors.grey,
+            blurRadius: 10.0,
+          ),
+        ],
+      ),
+      child: Column(
+        children: <Widget>[
+          Text(categoryName, style: titileStyleLighterBlack),
+          Container(
+            padding: EdgeInsets.only(top: 30),
+            height: 100,
+            width: 70,
+            child: FloatingActionButton(
+              backgroundColor: Colors.white,
+              child:  jobCatToIcon[categoryName],
+              elevation: 10,
+              onPressed: () {
+
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<Widget> getJobCategories() {
+    List<Widget> jobCategoriesCards = [];
+    List<Widget> rows = [];
+    int i = 0;
+    for (String category in jobCategories) {
+      if (i < 2) {
+        rows.add(getCategoryContainer(category));
+        i ++;
+      } else {
+        i = 0;
+        jobCategoriesCards.add(new Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: rows,
+        ));
+        rows = [];
+        rows.add(getCategoryContainer(category));
+        i++;
+      }
+    }
+    if (rows.length > 0) {
+      jobCategoriesCards.add(new Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: rows,
+      ));
+    }
+    return jobCategoriesCards;
+  }
+
+  List<Job> findJobs() {
+    List<Job> jobs = [];
+    for (int i = 0; i < 10; i++) {
+      jobs.add(new Job("Volvo", "Frontend Developer", 20000, "Remote", "Part time", new AssetImage("lib/assets/volvo.png")));
+    }
+    return jobs;
+  }
+
+  String makeSalaryToK(double salary) {
+    String money = "";
+    if (salary > 1000) {
+      if (salary > 100000000) {
+        salary = salary/100000000;
+        money = salary.toInt().toString() + "M";
+      } else {
+        salary = salary/1000;
+        money = salary.toInt().toString() + "K";
+      }
+    } else {
+      money = salary.toInt().toString();
+    }
+    return "\$" + money;
+  }
+
+  List<Widget> getRecentJobs() {
+    List<Widget> recentJobCards = [];
+    List<Job> jobs = findJobs();
+    for (Job job in jobs) {
+      recentJobCards.add(getJobCard(job));
+    }
+    return recentJobCards;
+  }
+
+  Widget getJobCard(Job job) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.only(right: 20, bottom: 30, top: 30),
+      height: 150,
+      width: 200,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            new BoxShadow(
+              color: Colors.grey,
+              blurRadius: 20.0,
+            ),
+          ],
+          borderRadius: BorderRadius.all(Radius.circular(15))
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: job.companyLogo,
+              ),
+              Text(
+                job.jobTitle,
+                style: jobCardTitileStyleBlue,
+              )
+            ],
+          ),
+          Text(job.companyName + " - " + job.timeRequirement, style: jobCardTitileStyleBlack),
+          Text(job.location),
+          Text(makeSalaryToK(job.salary), style: salaryStyle)
+        ],
+      ),
     );
   }
 }
